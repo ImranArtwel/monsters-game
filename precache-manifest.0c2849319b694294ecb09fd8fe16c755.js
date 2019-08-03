d37a87592831ab3aa676c5e46e0a101020202876
Artwel <acc.consultations.it@gmail.com>
2019-08-03 18:26:46 +0800

    Updates

---
+++ precache-manifest.0c2849319b694294ecb09fd8fe16c755.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca5c152917ada63e0e025792d533b388",
+    "url": "/monsters-game/index.html"
+  },
+  {
+    "revision": "2051dcc7e7a2bf3a908e",
+    "url": "/monsters-game/static/css/main.bc26ac44.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-game/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "2051dcc7e7a2bf3a908e",
+    "url": "/monsters-game/static/js/main.84cdfde5.chunk.js"
+  },
+  {
+    "revision": "cf220ebcb2c48dfa5979",
+    "url": "/monsters-game/static/js/runtime~main.341fa549.js"
+  }
+]);
